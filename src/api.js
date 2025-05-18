2feb47450acd2d6eda0c020059441ce704020090
LeLoustique <remi.gauthier1998@orange.fr>
2025-05-18 16:09:35 +0200

    config local

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const BACKEND_URL = "https://sping-ai-majordome.onrender.com"; 
+const BACKEND_URL = "https://sping-ai-majordome.onrender.com";
+//const BACKEND_URL = "http://localhost:8080";
 
 export async function sendMessageToChatController(message) {
   const response = await fetch(`${BACKEND_URL}/api/chat`, {
